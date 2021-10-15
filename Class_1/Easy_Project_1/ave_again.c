702c7c442c610cac2e9a0f72b1689cae1f454beb
21-boonam02 <boonam0417@gmail.com>
2021-10-15 18:02:15 +0900

    compiling and editing my workspace

---
+++ Class_1/Easy_Project_1/ave_again.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int sub_count;
+
+	scanf("%d", &sub_count);
+	float total_score = 0;
+	int score;
+	int max = 0;
+	for (int i = 1; i < sub_count + 1; i++) {
+		scanf("%d", &score);
+		total_score += score;
+
+		if (max < score) {
+			max = score;
+		}
+	}
+
+	float new_avg;
+	new_avg = ((total_score / max) * 100) / sub_count;
+	printf("%f", new_avg);
+}
