906daa0b35e4f1d0f03261bde1f8b36de0019f74
21-boonam02 <boonam0417@gmail.com>
2021-12-01 11:35:13 +0900

    But I succeed word assign with structure

--- Class_2_third/WordAssign.c
+++ Class_2_third/WordAssign.c
@@ -1,78 +1,93 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define _CRT_SECURE_NO_WARNINGS
 #define MAX_SIZE 20001
 
-//char sorted1[MAX_SIZE][51] = { 0 };
-//void merge(char list1[][50], int left, int mid, int right)
-//{
-//	int i = left, j = mid + 1, k = left, l;
-//	while (i <= mid && j <= right)
-//	{
-//		if (strlen(list1[i]) < strlen(list1[j]))
-//		{
-//			sorted1[k++][49] = list1[i++][49];
-//			
-//		}
-//		else if (strlen(list1[i]) > strlen(list1[j]))
-//		{
-//			sorted1[k++][49] = list1[j++][49];
-//		}
-//		else
-//		{
-//			if (strcmp(list1[i], list1[j]) > 0) sorted1[k++][49] = list1[j][49];
-//			else if (strcmp(list1[i], list1[j]) < 0) sorted1[k++][49] = list1[i][49];
-//			else sorted1[k++][49] = list1[i][49];
-//		}
-//
-//	}
-//
-//	if (i > mid)
-//	{
-//		for (l = j; l <= right; l++)
-//		{
-//			sorted1[k++][49] = list1[l++][49];
-//		}
-//	}
-//	else
-//	{
-//		for (l = i; l <= mid; l++)
-//		{
-//			sorted1[k++][49] = list1[l++][49];
-//		}
-//	}
-//
-//	for (l = left; l <= right; l++)
-//	{
-//		list1[l][49] = sorted1[l][49];
-//	}
-//}
-//void merge_sort(char list1[][50], int left, int right)
-//{
-//	int mid;
-//
-//	if (left < right)
-//	{
-//		mid = (left + right) / 2;
-//		merge_sort(list1,  left, mid);
-//		merge_sort(list1,  mid + 1, right);
-//		merge(list1, left, mid, right);
-//	}
-//}
+typedef struct 
+{
+	char word[51];
+	int count;
+}wor;
+
+wor sorted[MAX_SIZE];
+void merge(wor *list1, int left, int mid, int right)
+{
+	int i = left, j = mid + 1, k = left, l;
+	while (i <= mid && j <= right)
+	{
+		if (list1[i].count < list1[j].count)
+		{
+			sorted[k++] = list1[i++];
+		}
+		else if (list1[i].count > list1[j].count)
+		{
+			sorted[k++] = list1[j++];
+		}
+		else
+		{
+			if (strcmp(list1[i].word, list1[j].word) > 0)
+				sorted[k++] = list1[j++];
+			else 
+				sorted[k++] = list1[i++];
+		}
+	}
+
+	if (i > mid)
+	{
+		for (l = j; l <= right; l++)
+		{
+			sorted[k++] = list1[l];
+		}
+	}
+	else
+	{
+		for (l = i; l <= mid; l++)
+		{
+			sorted[k++] = list1[l];
+		}
+	}
+
+	for (l = left; l <= right; l++)
+	{
+		list1[l] = sorted[l];
+	}
+}
+void merge_sort(wor* list1, int left, int right)
+{
+	int mid;
+
+	if (left < right)
+	{
+		mid = (left + right) / 2;
+		merge_sort(list1,  left, mid);
+		merge_sort(list1,  mid + 1, right);
+		merge(list1, left, mid, right);
+	}
+}
+
+
 
 int main(void)
 {
 	int N;
-	char liss[][50] = { 0 };
 	scanf("%d", &N);
-
+	wor list[20001];
 	for (int i = 0; i < N; i++)
 	{
-		scanf("%s", liss[i]);
+		scanf("%s", list[i].word);
+		list[i].count = strlen(list[i].word);
 	}
-	//merge_sort(liss, 0, N - 1);
-	for (int k = 0; k < N; k++)
+
+	merge_sort(list, 0, N - 1);
+
+	printf("%s\n", list[0].word);
+	for (int k = 1; k < N; k++)
 	{
-		printf("%s\n", liss[k]);
+		if (strcmp(list[k - 1].word, list[k].word) != 0)
+		{
+			printf("%s\n", list[k].word);
+		}
 	}
+	return 0;
 }
