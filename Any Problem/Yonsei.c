e7747eb94b7a3fc30be15a2494781c1bf198da7c
21-boonam02 <boonam0417@gmail.com>
2022-01-05 00:45:23 +0900

    just simple problem

---
+++ Any Problem/Yonsei.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void)
+{
+	int N;
+	scanf("%d", &N);
+
+	if (N == 0) printf("YONSEI\n");
+	else if (N == 1)
+		printf("Leading the Way to the Future\n");
+}
