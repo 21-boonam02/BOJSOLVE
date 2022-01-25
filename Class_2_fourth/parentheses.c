fdc0ff80e209e72fedc33da6db714e4574539494
21-boonam02 <boonam0417@gmail.com>
2022-01-25 11:30:09 +0900

    I solved Parentheses!!

--- Class_2_fourth/parentheses.c
+++ Class_2_fourth/parentheses.c
@@ -2,18 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 #define _CRT_SECURE_NO_WARNINGS
-#define SIZE 100
+#define SIZE 51
 // (이게 나오면 일단 들어가고 ) 이게 나오면 이제 바로 바로 나오는 방식으로
 //하면 괜찮을 듯
-int full();
-void empty();
-void push(char x);
-void pop();
-void size();
-void top();
-// 아스키 코드 값 (는 72, ) 73이 각각 아스키 코드 값
-int TOP = -1;
-int stack[SIZE];
+
+//int full();
+//void empty();
+//void push(char x);
+//void pop();
+//void size();
+//void top();
+//// 아스키 코드 값 (는 72, ) 73이 각각 아스키 코드 값
+//int TOP = -1;
+//char stack[SIZE] = { 0, };
 
 int main(void)
 {
@@ -22,81 +23,102 @@ int main(void)
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 	{
+		int TOP = -1;
+		char stack[SIZE] = { 0, };
 		char word[51] = { 0, };
-		int num;
 		scanf("%s", &word);
+
 		for (int j = 0; j < strlen(word); j++)
 		{
-			if (word[j] == '(') push('(');
+			if (word[j] == '(') stack[++TOP] = '(';
 			else if (word[j] == ')')
 			{
-				for (int i = 0; i < 2; i++)
+				stack[++TOP] = ')';
+				if (stack[TOP- 1] == '(')
 				{
-					pop();
+					if (TOP == -1) break;
+					else
+					{
+						for (int i = 0; i < 2; i++)
+						{
+							TOP--;
+						}
+					}
 				}
 			}
 		}
-		empty();
-	}
-}
-//꽉 찼는지 확인하는 함수
-int full()
-{
-	if (TOP >= SIZE - 1)
-	{
-		//printf("Error : Stack is Full.\n ");
-		return 1;
-	}
-	return 0;
-}
-// 비어있는지 확인
-void empty()
-{
-	if (TOP == -1)
-	{
-		printf("YES\n");
-	}
-	else printf("NO\n");
-}
-void push(char x)
-{
-	if (!full())
-	{
-		stack[++TOP] = x;
-	}
-}
-//스택의 top에 있는 원소를 뺀다.
-void pop()
-{
-	if (TOP != -1)
-	{
-		char temp;
-		temp = stack[TOP];
-		TOP--;
-		//printf("%s\n", temp);
-	}
-	else printf("%d\n", -1);
-}
-// 지금 들어 있는 스택의 원소 개수를 확인
-void size()
-{
-	if (TOP != -1)
-	{
-		int cnt = 0;
-		for (int i = 0; i <= TOP; i++)
+		/*for (int k = 0; k < strlen(stack); k++)
 		{
-			cnt++;
+			printf("%c", stack[k]);
 		}
-		printf("%d\n", cnt);
+		printf("%d\n", TOP);
+		printf("\n");*/
+
+		if (TOP == -1)
+		{
+			printf("YES\n");
+		}
+		else printf("NO\n");
 	}
-	else printf("%d\n", 0);
 }
-//스택의 맨 꼭대기에 무엇이 있는지 확인
-void top()
-{
-	if (TOP != -1)
-	{
-		printf("%s\n", stack[TOP]);
-	}
-	else printf("%d\n", -1);
-}
+////꽉 찼는지 확인하는 함수
+//int full()
+//{
+//	if (TOP >= SIZE - 1)
+//	{
+//		//printf("Error : Stack is Full.\n ");
+//		return 1;
+//	}
+//	return 0;
+//}
+//// 비어있는지 확인
+//void empty()
+//{
+//	if (TOP == -1)
+//	{
+//		printf("YES\n");
+//	}
+//	else printf("NO\n");
+//}
+//void push(char x)
+//{
+//	if (!full())
+//	{
+//		stack[++TOP] = x;
+//	}
+//}
+////스택의 top에 있는 원소를 뺀다.
+//void pop()
+//{
+//	if (TOP != -1)
+//	{
+//		//char temp;
+//		//temp = stack[TOP];
+//		TOP--;
+//		//printf("%s\n", temp);
+//	}
+//	else printf("YES\n");
+//}
+//// 지금 들어 있는 스택의 원소 개수를 확인
+//void size()
+//{
+//	if (TOP != -1)
+//	{
+//		int cnt = 0;
+//		for (int i = 0; i <= TOP; i++)
+//		{
+//			cnt++;
+//		}
+//		printf("%d\n", cnt);
+//	}
+//	else printf("%d\n", 0);
+//}
+////스택의 맨 꼭대기에 무엇이 있는지 확인
+//void top()
+//{
+//	if (TOP != -1)
+//	{
+//		printf("%s\n", stack[TOP]);
+//	}
+//	else printf("%d\n", -1);
+//}
