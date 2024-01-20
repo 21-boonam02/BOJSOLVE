907d642628db163a9c66ff68f4987ca4fdd11514
21-boonam02 <81502686+21-boonam02@users.noreply.github.com>
2024-01-20 22:49:55 +0900

    Create sol2n_Tile.py

---
+++ pythonfiles/sol2n_Tile.py
@@ -0,0 +1,12 @@
+def sol2n_Tile():
+    n = int(input())
+    dp = [0] * 1001
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3,1001):
+        dp[i] = (dp[i -1] + dp[i - 2]) % 10007
+    print(dp[n])
+
+
+if __name__ == "__main__":
+    sol2n_Tile()
